3b5d9efcd7548d8b0ca0e75e84ecca448fc7e86b
NoviYantiLaurensia <noviyantilaurensia4@gmail.com>
2022-06-21 20:12:36 +0700

    Initial commit

---
+++ app/src/main/java/com/program/dpm/DataLecturer.kt
@@ -0,0 +1,11 @@
+package com.program.dpm
+
+class DataLecturer (
+    var nidn: String,
+    var namaDosen: String,
+    var jabatan: String,
+    var golongan: String,
+    var pendidikan: String,
+    var keahlian: String,
+    var programstudi: String
+)
